694b6ebe834f5c5b83ca2db626a304ea5c69d5f0
13angs <13angz.um@gmail.com>
2023-01-16 16:24:54 +0000

    convert to NewtonSoft.Json

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,8 @@ List<Todo> todos = new List<Todo>();
 app.MapPost("/todo", ([FromBody] object content)=> {
     // var request = Requ
     // todos.Add(todo);
-    Todo? todo = JsonSerializer.Deserialize<Todo>(content.ToString()!);
+    // Todo? todo = JsonSerializer.Deserialize<Todo>(content.ToString()!);
+    Todo? todo = JsonConvert.DeserializeObject<Todo>(content.ToString()!);
 
     todos.Add(todo!);
     return todos;
